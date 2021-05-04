f6134a5d732ee8e633351fc265ce6df17c2e11da
TruongHa <truonghq.dev@gmail.com>
2021-05-04 15:07:25 +0700

    add file main and trip detail

---
+++ TripControl/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  TripControl
+//
+//  Created by AUTO SERVER on 04/05/2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var tableviewTrips: UITableView!
+    
+    @IBOutlet weak var vwAddButton: UIView!
+    @IBOutlet weak var btnAddTrip: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func addTripTapped(_ sender: Any) {
+        
+    }
+    
+
+}
